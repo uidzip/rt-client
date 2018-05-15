0a8277b81b8427e2472bb78c8393b50caa1e105e
runner <runner@security.berkeley.edu>
2018-05-15 15:17:05 -0700

    sync gem version in User-Agent header

--- rt_client.rb
+++ rt_client.rb
@@ -38,7 +38,7 @@ require 'pp'
 
 class RT_Client
 
-  UA = "Mozilla/5.0 ruby RT Client Interface 1.0.0"
+  UA = "Mozilla/5.0 ruby RT Client Interface 1.0.1"
   attr_reader :status, :site, :version, :cookies, :server, :user, :cookie
 
   # Create a new RT_Client object. Load up our stored cookie and check it.
